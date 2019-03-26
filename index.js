3b0fcb90653162e548c86e9d4f5f8387401a39ce
Kerben Bermingham <b.kerben2@gmail.com>
2019-03-26 00:25:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ function onKeyDown(event) {
     const currentPosition = parseInt(dodger.style.left)
     dodger.style.left = currentPosition - 5 + 'px'
   } else if (event.keyCode === 39) {
-    const currentPosition = parseInt(dodger.style.left)
-    dodger.style.left = currentPosition + 5 + 'px'
+    moveDodgerRight()
   }
 }
 
+function moveDodgerRight() {
+const currentPosition = parseInt(dodger.style.left)
+    dodger.style.left = currentPosition + 5 + 'px'
+}
 // 2 - Add the event listener
 document.addEventListener('keydown', onKeyDown)
