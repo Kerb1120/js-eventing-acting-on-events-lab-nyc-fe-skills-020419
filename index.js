fb0671cab0edbbf30416b236384f37fc4a032e95
Kerben Bermingham <b.kerben2@gmail.com>
2019-03-26 00:33:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ function onKeyDown(event) {
   if (event.keyCode === 37) {
     // how do we actually move this left  
     const currentPosition = parseInt(dodger.style.left)
+    if (currentPosition - 5 >= 0) {
     dodger.style.left = currentPosition - 5 + 'px'
-  } else if (event.keyCode === 39) {
+    }  
+    } else if (event.keyCode === 39) {
     moveDodgerRight()
   }
 }
