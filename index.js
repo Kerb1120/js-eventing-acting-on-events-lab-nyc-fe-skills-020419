96897287c0267d4a208b8175460f3a816f2a7f17
Kerben Bermingham <b.kerben2@gmail.com>
2019-03-26 00:14:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const dodger = document.getElementById('dodger')
 
 function onKeyDown(event) {
-  if(event.keyCode === 37) {
-  console.log(dodger.style.left)
+  if (event.keyCode === 37) {
+    // how do we actually move this left  
+    console.log(dodger.style.left)
   }
 }
 
+// 2 - Add the event listener
 document.addEventListener('keydown', onKeyDown)
