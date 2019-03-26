7c7dc42389dd07d09c268b10c01a7dec5eaac627
Kerben Bermingham <b.kerben2@gmail.com>
2019-03-26 00:37:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function onKeyDown(event) {
 
 function moveDodgerRight() {
 const currentPosition = parseInt(dodger.style.left)
-     if (currentPosition + 5 <= 359)
+     if (currentPosition + 5 <= 360)
     dodger.style.left = currentPosition + 5 + 'px'
 }
 // 2 - Add the event listener
